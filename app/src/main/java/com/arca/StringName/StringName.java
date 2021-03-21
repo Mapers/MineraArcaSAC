7ed41b478548bbbc3cca01856af082f3bcb2619f
Manuel Fernandez <kiramapers@gmail.com>
2021-03-20 20:07:44 -0500

    Modificacion

--- app/src/main/java/com/arca/StringName/StringName.java
+++ app/src/main/java/com/arca/StringName/StringName.java
@@ -1,16 +1,16 @@
 package com.arca.StringName;
 
 public class StringName {
-
+    // Clases publicas
    // public static String URL_API_Arca ="http://app-arca.online";
    // public static String URL_API_Arca ="http://192.119.107.41"; // Antigua URL
     public static String URL_API_Arca ="http://190.119.255.59:8080/app-arca";
     public static String id_nivel_Usuario_Mostrar_Menu_Navegacion ="1";
     public static String id_nivel_Usuario_Mostrar_Menu_Navegacion_2 ="2";
    // public  static  String Historial_Ordenes="http://192.119.107.41/listaocurrenciaswv.php"; Antigua URL
-   public  static  String Historial_Ordenes="http://190.119.255.59:8080/app-arca/listaocurrenciaswv.php";
+    public  static  String Historial_Ordenes="http://190.119.255.59:8080/app-arca/listaocurrenciaswv.php";
   //  public  static  String Historial_Aprobar="http://192.119.107.41/ordenespendienteswv.php";Antigua URL
-  public  static  String Historial_Aprobar="http://190.119.255.59:8080/app-arca/ordenespendienteswv.php";
+   public  static  String Historial_Aprobar="http://190.119.255.59:8080/app-arca/ordenespendienteswv.php";
    // public  static  String Historial_Ordenes_AProadas="http://app-arca.online/detalleocurrenciaswv.php";
    // public  static  String Historial_Ordenes_AProadas="http://192.119.107.41/historialordenesaprobadas.php";;Antigua URL
    public  static  String Historial_Ordenes_AProadas="http://190.119.255.59:8080/app-arca/historialordenesaprobadas.php";
